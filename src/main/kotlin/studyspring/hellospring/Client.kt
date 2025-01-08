463a576dc65d5932e210ecd0e64217266b070e0c
dawit <dawit0310@gmail.com>
2025-01-08 17:21:48 +0900

    refactor: move runtime dependency management to caller classes 런타임 의존관계의 책임을 PaymentService.kt -> client.main 으로 변

--- src/main/kotlin/studyspring/hellospring/Client.kt
+++ src/main/kotlin/studyspring/hellospring/Client.kt
@@ -5,6 +5,7 @@ import java.math.BigDecimal
 class Client
 
 fun main(args: Array<String>) {
-    val payment = PaymentService().prepare(100L, "USD", BigDecimal(50.7))
+    val paymentService = PaymentService(WebAPIExRateProvider())
+    val payment = paymentService.prepare(100L, "USD", BigDecimal(50.7))
     println(payment)
 }
--- src/main/kotlin/studyspring/hellospring/PaymentService.kt
+++ src/main/kotlin/studyspring/hellospring/PaymentService.kt
@@ -5,7 +5,11 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 class PaymentService {
-    private val exRateProvider: ExRateProvider = WebAPIExRateProvider()
+    private val exRateProvider: ExRateProvider
+
+    constructor(exRateProvider: ExRateProvider) {
+        this.exRateProvider = exRateProvider
+    }
 
     fun prepare(orderId: Long, currency: String, foreignCurrencyAmount: BigDecimal): Payment {
         val exRate: BigDecimal = exRateProvider.getExRate(currency)
